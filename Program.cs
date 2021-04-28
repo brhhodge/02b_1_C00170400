0431a6e7221ad6668aaba0d0758afc8a8c0fd2b8
Brian Hodge <brianrhodge@gmail.com>
2021-04-28 05:58:25 -0500

    Added .csproj, obj/, and bin/

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//Brian Hodge
+//C00170400
+//CMPS 358
+//assignment 02b_1
+
+using System;
+
+namespace _02b_1_C00170400
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter package weight: ");
+            double weight = Double.Parse(Console.ReadLine());
+
+            if (weight >= 50){
+                Console.WriteLine("Cannot ship package. The package weight cannot exceed 50 kilograms.");
+            }
+            else if (weight <= 0) {
+                Console.WriteLine("Data Error.");
+            }
+            else {
+                Console.WriteLine($"The shipping cost for a package weighing {weight} kilograms is: ${shipCost(weight)}");
+            }
+        }
+
+        static double shipCost(double w) {
+
+            double shippingPrice = 0;
+
+            if (w > 0) {
+                if (w <= 1) {
+                    shippingPrice = 3.50;
+                } 
+                else if (w > 1) {
+                    if (w <= 3) {
+                        shippingPrice = 5.50;
+                    }
+                    else if (w > 3) {
+                        if (w <= 10) {
+                            shippingPrice = 8.50;
+                        }
+                        else if (w < 50) {
+                            shippingPrice = 13.50;
+                        } 
+                    } 
+                }
+
+            } return shippingPrice;
+        }
+    }
+}
